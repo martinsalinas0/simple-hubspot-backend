2c230e3c65d9d655a9c36edf3017c8ae57b3bcfd
martinsalinas0 <martinlsalinas@pm.me>
2025-06-10 21:05:54 -0500

    created the routes for companies

---
+++ routes/company.routes.js
@@ -0,0 +1,16 @@
+import express from 'express'
+import {
+  getCompanies,
+  addCompany,
+  deleteComp,
+  updateComp,
+} from '../controllers/companies.controllers.js'
+
+const router = express.Router();
+
+router.get('/companies', getCompanies);
+router.post('/companies', addCompany);
+router.delete('/companies/:id', deleteComp);
+router.put('/companies/:id', updateComp);
+
+module.exports = router;
