9952edcffb59e99ad3d45ec7116d0f32ab1e86c3
Martin <martinlsalinas@pm.me>
2025-06-12 23:14:08 -0500

    added validation for MONGODB_URI

--- config/db.js
+++ config/db.js
@@ -1,13 +1,22 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
 export default async function connectDB() {
   const connectString = process.env.MONGODB_URI;
 
+
+  if(!connectString) { 
+      console.error("MONGODB_URI is not defined in environment variables"); 
+      process.exit(1); 
+  } 
+
   try {
     const conn = await mongoose.connect(connectString);
-    console.log(`MongoDB connected. Connected to database: ${connectString}`);
+    console.log(`MongoDB connected. Connected to database: ${connectString} and ${conn.connection.name}`);
   } catch (error) {
-    console.error(`Errorjjk: ${error.message}`);
+    console.error(`Database Error: ${error.message}`);
     process.exit(1);
   }
 }
+
